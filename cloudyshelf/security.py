33b96d6c244d43c165e4a1012a137cb3b5a49284
Nicholas Pilon <npilon@lexmachina.com>
2012-02-19 14:10:00 -0800

    Oh, and include the file with the actual security bits.

---
+++ cloudyshelf/security.py
@@ -0,0 +1,30 @@
+from pyramid.security import (
+    Allow,
+    Authenticated,
+    Deny,
+    Everyone,
+)
+
+from .models import (
+	DBSession,
+	User
+)
+
+class SimpleContext(object):
+    def __init__(self, request):
+        pass
+
+    __acl__ = [
+        (Allow, 'group:dropbox', 'dropbox'),
+        (Allow, Authenticated, 'logged in'),
+    ]
+
+def session_callback(userid, request):
+    user = DBSession.query(User).get(userid)
+    if not user:
+    	return None
+    request.user = user
+    principals = [userid,]
+    if user.dropbox_token:
+    	principals.append('group:dropbox')
+    return principals
